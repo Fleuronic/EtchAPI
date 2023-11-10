9922db60c3a2315da08e2a07923f1c533e55e7c3
Jordan Kay <jordan@fleuronic.com>
2023-11-10 07:27:47 -0600

    Add API

---
+++ Sources/EtchAPI/API.swift
@@ -0,0 +1,45 @@
+// Copyright © Fleuronic LLC. All rights reserved.
+
+import enum Catenary.Request
+import struct Foundation.URL
+import class Foundation.JSONDecoder
+import class Foundation.DateFormatter
+import protocol Catenary.API
+import protocol Caesura.HasuraAPI
+
+public struct API {
+	let apiKey: String
+
+	public init(apiKey: String) {
+		self.apiKey = apiKey
+	}
+}
+
+// MARK: -
+extension API: HasuraAPI {
+	// MARK: API
+	public var baseURL: URL {
+		URL(string: "https://api.linear.app/graphql")!
+	}
+
+	public var authenticationHeader: Request.Header? {
+		.init(
+			field: "Authorization",
+			value: "Bearer \(apiKey)"
+		)
+	}
+
+	public var decoder: JSONDecoder {
+		let decoder = JSONDecoder()
+		decoder.keyDecodingStrategy = .convertFromSnakeCase
+		decoder.dateDecodingStrategy = .formatted(dateFormatter)
+		return decoder
+	}
+}
+
+// MARK: -
+private let dateFormatter = {
+	let formatter = DateFormatter()
+	formatter.dateFormat = "YYYY-MM-dd"
+	return formatter
+}()
